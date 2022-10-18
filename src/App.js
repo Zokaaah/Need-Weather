934b4e74c4f9feea314402a21ee11954cdc5dff2
Zokah <gabriel.oliveira_012@hotmail.com>
2022-10-17 21:41:16 -0300

    Commit inicial do sistema

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import bkg from "./images/bkg.jpg"
 
+import React, {useState} from 'react'
+import styles from '../src/Styles/index.module.css'
+
+const api = {
+  key: "fd1e121652c7ab46db85dd2da4181d31",
+
+  baseURL: "https://api.openweathermap.org/data/2.5/"
+}
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+  const date = new Date().toLocaleString();
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.baseURL}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        {(typeof weather.main != "undefined") ?(
+                  <div className={styles.local_container}>
+                <div className={styles.local}>{weather.name}, {weather.sys.country}</div>
+                <div className={styles.data}>{date}</div>
+            </div>
+):('')}
+
+            <div className={styles.search_box}>
+                <input type="text"
+                 className={styles.search_bar}
+                placeholder='Digite o nome da cidade'
+                onChange={e => setQuery(e.target.value)}
+                value={query}
+                onKeyUp={search}
+                   />
+            </div>
+
+           
+            {(typeof weather.main != "undefined") ?(
+
+            <div className={styles.Weather_container}>
+                <div className={styles.temp}>{Math.round(weather.main.temp)}{"°"}</div>
+                <div className={styles.weather}>{weather.weather[0].main}</div>
+
+            </div>
+            ):('')}
+        </main>
     </div>
   );
 }
---
+++ src/http/api.js
@@ -0,0 +1,19 @@
+import React from "react";
+import axios from "axios"
+import { useState } from "react";
+
+const api = axios.create ({
+
+    key: "fd1e121652c7ab46db85dd2da4181d31",
+    baseURL:"https://api.openweathermap.org/data/2.5/weather?lat={44.55}&lon={55.77}&appid={${key}}}"
+    // fd1e121652c7ab46db85dd2da4181d31
+
+//     //  "lon": 10.99,
+//         "lat": 44.34
+ })
+
+
+
+
+
+export default api;
