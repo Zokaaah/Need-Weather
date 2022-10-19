fc09f763a7afa2bb9c6c7e8f0e4de8d5d372c4ea
Zokah <gabriel.oliveira_012@hotmail.com>
2022-10-18 21:21:54 -0300

    Adição de icones e tradução de condição climática

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 import bkg from "./images/bkg.jpg"
+import clouds from "./images/clouds.png"
+import sunny from "./images/sunny.png"
+import rainy from "./images/rainy.png"
+
 
 import React, {useState} from 'react'
 import styles from '../src/Styles/index.module.css'
@@ -25,6 +29,36 @@ function App() {
         });
     }
   }
+  function climate (){
+    const rainDay = weather.weather[0].main
+    const sunnyDay = weather.weather[0].main
+    const cloudDay = weather.weather[0].main
+
+    if (cloudDay === "Clouds"){
+      console.log("Nublado")
+      return <div><img className={styles.icons} src={clouds} alt="" />
+      <p>Nublado</p>
+      </div>
+    }
+    else if(sunnyDay === "Clear"){
+      return <div>
+        <img className={styles.icons} src={sunny} alt="" />
+        <p>Limpo</p>
+
+      </div>
+
+    }
+
+    else if (rainDay === "Rain"){
+      return <div>
+      <img className={styles.icons} src={rainy} alt="" />
+      <p>Chuvoso</p>
+
+    </div>
+
+    }
+  }
+  
 
  
   return (
@@ -33,9 +67,9 @@ function App() {
         {(typeof weather.main != "undefined") ?(
                   <div className={styles.local_container}>
                 <div className={styles.local}>{weather.name}, {weather.sys.country}</div>
-                <div className={styles.data}>{date}</div>
+                {/* <div className={styles.data}>{date}</div> */}
             </div>
-):('')}
+):(<div className={styles.select_city}><h1>Selecione a cidade</h1></div>)}
 
             <div className={styles.search_box}>
                 <input type="text"
@@ -47,15 +81,22 @@ function App() {
                    />
             </div>
 
+
+          
            
             {(typeof weather.main != "undefined") ?(
 
             <div className={styles.Weather_container}>
-                <div className={styles.temp}>{Math.round(weather.main.temp)}{"°"}</div>
-                <div className={styles.weather}>{weather.weather[0].main}</div>
+                <div className={styles.temp}>{Math.round(weather.main.temp)}{"°C"}
+                  <p className={styles.temp_max}>Temp Max {weather.main.temp_max}</p>
+                  <p className={styles.temp_min}>Temp Min {weather.main.temp_min}</p>
+
+                </div>
+                <div className={styles.weather}>{climate()}</div>
 
             </div>
             ):('')}
+
         </main>
     </div>
   );
